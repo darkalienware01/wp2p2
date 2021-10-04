9e3b79184b700f66459f02f5acae0ccca797ad88
darkalienware01 <yuzudook@gmail.com>
2021-10-04 15:47:27 +0700

    Update Pertemuan 4

---
+++ application/views/v_header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title> Web Prog II | Merancang template sederhana dengan Codeigniter 3 </title>
+	<link rel="stylesheet" type="text/css" href="<?= base_url();?>assets/css/stylep4.css">
+</head>
+
+<body>
+	<div id="wrapper">
+		<header>
+			<hgroup>
+				<h1> RentalBuku.net </h1>
+				<h3> Membuat Template Sederhana dengan CodeIgniter 3 </h3>
+			</hgroup>
+			<nav>
+				<ul>
+					<li> <a href="<?= base_url();?>index.php/P4"> Home </a> </li>
+					<li> <a href="<?= base_url();?>index.php/P4/about"> About </a> </li>
+				</ul>
+			</nav>
+			<div class="clear"></div>
+		</header>
+		
